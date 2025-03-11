934fda9855de72318e564a9b3e648459125622eb
Maciej Potręć <potrec123@proton.me>
2025-03-12 00:19:35 +0100

    feat: init app

---
+++ app/app/Http/Controllers/Pet/PetController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Http\Controllers\Pet;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class PetController extends Controller
+{
+    public function index()
+    {
+        
+    }
+}
