800f656b23a1e3b2a93088756e6c9142c5116d2b
Maciej Potręć <potrec123@proton.me>
2025-03-14 00:03:01 +0100

    feat: add redirect to the pets index

--- app/routes/web.php
+++ app/routes/web.php
@@ -5,14 +5,18 @@ use Inertia\Inertia;
 use App\Http\Controllers\Pet\PetController;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
+    return redirect()->route('pets.index');
 })->name('home');
 
 Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
+    return redirect()->route('pets.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::resource('pets', PetController::class);
+
+Route::fallback(function () {
+    return redirect()->route('pets.index');
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
